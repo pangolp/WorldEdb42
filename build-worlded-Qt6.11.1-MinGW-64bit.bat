f118ac3df568da7973a7d0faf8d62f42fb5bfa89
Pagani Walter <paganiwalter@gmail.com>
2026-07-04 02:36:16 -0300

    Remove obsolete qmake build scripts
    build-worlded-Qt6.11.1-MSVC-2022-64bit.bat, build-worlded-Qt6.11.1-MinGW-64bit.bat,
    and run-build-MinGW.ps1 are superseded by the CMake build system and contained
    machine-specific hardcoded paths.

--- build-worlded-Qt6.11.1-MinGW-64bit.bat
+++
@@ -1,5 +0,0 @@
-set PATH=C:\Programming\QtSDK2015\Tools\mingw1310_64\bin;%PATH%
-if not exist "C:\Programming\PZWorldEd\dist64" mkdir "C:\Programming\PZWorldEd\dist64"
-cd C:\Programming\PZWorldEd\dist64
-"C:\Programming\QtSDK2015\6.11.1\mingw_64\bin\qmake.exe" "C:\Users\Walter\Desktop\WorldEdb42\PZWorldEd.pro" -r -spec win32-g++ "CONFIG+=release"
-"C:\Programming\QtSDK2015\Tools\mingw1310_64\bin\mingw32-make.exe" -j4
