c16e338108df14403ed0446e8caebc716f9eb5c1
Pagani Walter <paganiwalter@gmail.com>
2026-07-07 23:25:39 -0300

    InGameMap: fix indentation in writeFeature() (#11)

--- src/editor/InGameMap/ingamemapwriterbinary.cpp
+++ src/editor/InGameMap/ingamemapwriterbinary.cpp
@@ -134,23 +134,21 @@ public:
     void writeFeature(QDataStream &w, InGameMapFeature* feature)
     {
         SaveStringIndex(w, feature->mGeometry.mType);
-        
-            w << qint8(feature->mGeometry.mCoordinates.size());
-            for (auto& coords : feature->mGeometry.mCoordinates) {
-                w << qint16(coords.size());
-                for (auto& point : coords) {
 
-                    w << qint16(int(point.x));
-                    w << qint16(int(point.y));
-                }
+        w << qint8(feature->mGeometry.mCoordinates.size());
+        for (auto& coords : feature->mGeometry.mCoordinates) {
+            w << qint16(coords.size());
+            for (auto& point : coords) {
+                w << qint16(int(point.x));
+                w << qint16(int(point.y));
             }
+        }
 
-            w << qint8(feature->mProperties.size());
-            for (auto& property : feature->mProperties) {
-                SaveStringIndex(w, property.mKey);
-                SaveStringIndex(w, property.mValue);
-            }
-        
+        w << qint8(feature->mProperties.size());
+        for (auto& property : feature->mProperties) {
+            SaveStringIndex(w, property.mKey);
+            SaveStringIndex(w, property.mValue);
+        }
     }
 
     void SaveString(QDataStream& w, const QString& str)
